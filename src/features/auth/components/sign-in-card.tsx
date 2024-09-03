4e760e67200a82a45e1715065c7a7e647032b57a
ywx <540040588@qq.com>
2024-09-03 17:42:56 +0800

    feat: add convex auth

--- src/features/auth/components/sign-in-card.tsx
+++ src/features/auth/components/sign-in-card.tsx
@@ -1,4 +1,7 @@
+import { useState } from "react";
 import { FcGoogle } from "react-icons/fc";
+import { FaGithub } from "react-icons/fa";
+import { useAuthActions } from "@convex-dev/auth/react";
 
 import { Button } from "@/components/ui/button";
 import {
@@ -10,13 +13,27 @@ import {
 } from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
 import { Separator } from "@/components/ui/separator";
-import { FaGithub } from "react-icons/fa";
 
-const SignInCard = () => {
+import { SignInFlow } from "../types";
+
+interface SignInCardProps {
+  setState: (state: SignInFlow) => void;
+}
+
+const SignInCard = ({ setState }: SignInCardProps) => {
+  const { signIn } = useAuthActions();
+
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleProviderSignIn = (value: "github" | "google") => {
+    signIn(value);
+  };
+
   return (
     <Card className="h-full w-full p-8">
       <CardHeader className="px-0 pt-0">
-        <CardTitle>Login to continue</CardTitle>
+        <CardTitle>Sign in to continue</CardTitle>
         <CardDescription>
           Use your email or another service to continue
         </CardDescription>
@@ -25,16 +42,16 @@ const SignInCard = () => {
         <form className="space-y-2.5">
           <Input
             disabled={false}
-            value=""
-            onChange={() => {}}
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
             placeholder="Email"
             type="email"
             required
           />
           <Input
             disabled={false}
-            value=""
-            onChange={() => {}}
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
             placeholder="Password"
             type="password"
             required
@@ -52,20 +69,29 @@ const SignInCard = () => {
             size="lg"
             className="relative w-full"
           >
-            <FcGoogle className="size-5 absolute top-2.5 left-2.5" />
+            <FcGoogle className="size-5 absolute top-3 left-2.5" />
             Continue with Google
           </Button>
           <Button
             disabled={false}
-            onClick={() => {}}
+            onClick={() => handleProviderSignIn("github")}
             variant="outline"
             size="lg"
             className="relative w-full"
           >
-            <FaGithub className="size-5 absolute top-2.5 left-2.5" />
+            <FaGithub className="size-5 absolute top-3 left-2.5" />
             Continue with Github
           </Button>
         </div>
+        <p className="text-xs text-muted-foreground">
+          Don&apos;t have an account?
+          <span
+            onClick={() => setState("signUp")}
+            className="text-sky-700 hover:underline cursor-pointer pl-2"
+          >
+            Sign up
+          </span>
+        </p>
       </CardContent>
     </Card>
   );
