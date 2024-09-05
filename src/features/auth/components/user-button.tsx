3513fd81f07feff819ff76e782ed65b24209d057
ywx <540040588@qq.com>
2024-09-05 17:08:03 +0800

    feat: add convex user-button

---
+++ src/features/auth/components/user-button.tsx
@@ -0,0 +1,47 @@
+"use client";
+import { Loader, LogOut } from "lucide-react";
+import { useAuthActions } from "@convex-dev/auth/react";
+
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { useCurrentUser } from "../hooks/use-current-user";
+
+const UserButton = () => {
+  const { signOut } = useAuthActions();
+  const { data, isLoading } = useCurrentUser();
+
+  if (isLoading)
+    return <Loader className="size-4 animate-spin text-muted-foreground" />;
+
+  if (!data) return null;
+
+  const { image, name } = data;
+
+  const avatarFallback = name!.charAt(0).toUpperCase();
+
+  return (
+    <DropdownMenu modal={false}>
+      <DropdownMenuTrigger className="outline-none relative">
+        <Avatar className="size-10 hover:opacity-75 transition">
+          <AvatarImage alt={name} src={image} />
+          <AvatarFallback className="bg-sky-500 text-white">
+            {avatarFallback}
+          </AvatarFallback>
+        </Avatar>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent className="w-60" align="center" side="right">
+        <DropdownMenuItem className="cursor-pointer h-10" onClick={signOut}>
+          <LogOut className="size-4 mr-2" />
+          Log out
+        </DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
+};
+
+export default UserButton;
