308e35bc735a524a2cf0242dc4dcc7d1601741d6
yangwenxuan <540040588@qq.com>
2024-09-07 22:41:48 +0800

    feat: add sidebar buttons & define members table

--- src/app/workspace/[workspaceId]/sidebar.tsx
+++ src/app/workspace/[workspaceId]/sidebar.tsx
@@ -1,10 +1,37 @@
+'use client'
+
 import UserButton from "@/features/auth/components/user-button";
 import { WorkspaceSwitcher } from "./workspace-switcher";
+import { SidebarButton } from "./sidebar-button";
+import { Bell, Home, MessageSquare, MoreHorizontal } from "lucide-react";
+import { usePathname } from "next/navigation";
 
 export const Sidebar = () => {
+  const pathname = usePathname();
+
   return (
     <aside className="w-[70px] h-full bg-[#481349] flex flex-col gap-y-4 items-center pt-[9px] pb-[4px]">
       <WorkspaceSwitcher />
+      <SidebarButton
+        icon={Home}
+        label="Home"
+        isActive={pathname.includes("/workspace")}
+      />
+      <SidebarButton
+        icon={MessageSquare}
+        label="DMs"
+        isActive={pathname.includes("/workspace")}
+      />
+      <SidebarButton
+        icon={Bell}
+        label="Activity"
+        isActive={pathname.includes("/workspace")}
+      />
+      <SidebarButton
+        icon={MoreHorizontal}
+        label="More"
+        isActive={pathname.includes("/workspace")}
+      />
       <div className="flex flex-col items-center justify-center gap-y-1 mt-auto">
         <UserButton />
       </div>
