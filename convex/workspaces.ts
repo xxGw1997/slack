5c875dbc99159f6cd425c88f62ccd12cf82a652d
yangwenxuan <540040588@qq.com>
2024-09-08 17:25:04 +0800

    feat: get workspace by members & add generateCode

--- convex/workspaces.ts
+++ convex/workspaces.ts
@@ -3,29 +3,39 @@ import { v } from "convex/values";
 import { mutation, query } from "./_generated/server";
 import { getAuthUserId } from "@convex-dev/auth/server";
 
+const generateCode = () => {
+  const code = Array.from(
+    { length: 6 },
+    () => "0123456789abcdefghijklmnopqrstuvwxyz"[Math.floor(Math.random() * 36)]
+  ).join("");
+
+  return code;
+};
+
 export const get = query({
   args: {},
   handler: async (ctx, args) => {
-    return await ctx.db.query("workspaces").collect();
-  },
-});
-
-export const create = mutation({
-  args: { name: v.string() },
-  handler: async (ctx, args) => {
     const userId = await getAuthUserId(ctx);
-    if (!userId) throw new Error("Unauthorized!");
+    if (!userId) {
+      return [];
+    }
 
-    //TODO: Create a proper method later
-    const joinCode = "123456";
+    const members = await ctx.db
+      .query("members")
+      .withIndex("by_user_id", (q) => q.eq("userId", userId))
+      .collect();
+    const workspaceIds = members.map((member) => member.workspaceId);
 
-    const workspaceId = await ctx.db.insert("workspaces", {
-      name: args.name,
-      userId,
-      joinCode,
-    });
+    const workspaces = [];
 
-    return workspaceId;
+    for (const workspaceId of workspaceIds) {
+      const workspace = await ctx.db.get(workspaceId);
+      if (workspace) {
+        workspaces.push(workspace);
+      }
+    }
+
+    return workspaces;
   },
 });
 
@@ -36,6 +46,40 @@ export const getById = query({
 
     if (!userId) throw new Error("Unauthorized!");
 
+    const member = await ctx.db
+      .query("members")
+      .withIndex("by_workspace_id_user_id", (q) =>
+        q.eq("workspaceId", args.id).eq("userId", userId)
+      )
+      .unique();
+
+    if (!member) return null;
+
     return await ctx.db.get(args.id);
+  },
+});
+
+export const create = mutation({
+  args: { name: v.string() },
+  handler: async (ctx, args) => {
+    const userId = await getAuthUserId(ctx);
+    if (!userId) throw new Error("Unauthorized!");
+
+    //TODO: Create a proper method later
+    const joinCode = generateCode();
+
+    const workspaceId = await ctx.db.insert("workspaces", {
+      name: args.name,
+      userId,
+      joinCode,
+    });
+
+    await ctx.db.insert("members", {
+      userId,
+      workspaceId,
+      role: "admin",
+    });
+
+    return workspaceId;
   },
 });
